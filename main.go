eca9030b9fef55474a12cd0c37195984e0a28c00
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-11-11 23:17:41 -0500

    Changing code and processing in batches

--- main.go
+++ main.go
@@ -10,41 +10,53 @@ import (
 	"sync"
 )
 
-func execute() {
+func executeBatch(minP, maxP int) {
 	//fmt.Println(testPortConnection(os.Args[1], "22"))
-	maxPort := 100
-	minPort := 1
+	maxPort := maxP
+	minPort := minP
 	var wg sync.WaitGroup
 	wg.Add((maxPort - minPort + 1))
 	for run := minPort; run <= maxPort; run++ {
-		go func(host, port string) string {
+		go func(host, port string, wg *sync.WaitGroup) {
 			if testPortConnection(host, port) == true {
 				fmt.Println(("Port " + port + " OPEN"))
-				wg.Done()
-				return ("Port " + port + " OPEN")
+			} else {
+				fmt.Println(("Port " + port + " CLOSED"))
 			}
-			fmt.Println(("Port " + port + " CLOSED"))
 			wg.Done()
-			return ("Port " + port + " CLOSED")
-		}(os.Args[1], strconv.FormatInt(int64(run), 10))
+		}(os.Args[1], strconv.FormatInt(int64(run), 10), &wg)
 		//testPortConnection(os.Args[1], strconv.FormatInt(int64(run), 10))
 	}
 	wg.Wait()
 }
 
+func execute() {
+	batchSize := 200
+	for run := 1; run < 65536; run++ {
+		higherEnd := run + batchSize
+		if (higherEnd) > 65535 {
+			higherEnd = 65535
+		}
+		executeBatch(run, higherEnd)
+		run = run + batchSize
+	}
+}
+
 func testPortConnection(host, port string) bool {
-	fmt.Println("Testing connection...." + port)
-	//out, err := exec.Command("ls").Output()
-	out, err := exec.Command("timeout", "1", "telnet", host, port).Output()
+	//fmt.Println("Testing connection...." + port)
+	out, err := exec.Command("timeout", "5", "nc", "-z", "-v", host, port).Output()
 	if err != nil {
 		//fmt.Println("Coming in error for PORT " + port)
 		//fmt.Printf("%s", err)
+		if strings.HasSuffix(string(err.Error()), "exit status 1") {
+			return true
+		}
+		if strings.HasSuffix(string(err.Error()), "exit status 124") {
+			return false
+		}
 	}
 	fmt.Println(string(out))
-	if strings.HasSuffix(string(err.Error()), "exit status 1") {
-		return true
-	}
-	return false
+	return true
 }
 
 func main() {
